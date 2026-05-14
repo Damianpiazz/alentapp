e63f0d5ec7be6c787e25ac17a75081b573c2a99c
Manuela Chanquía <manuchanquia04@gmail.com>
2026-05-14 19:52:23 -0300

    feat(payment): define peyment types y dtos en shared

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -5,29 +5,54 @@ export type MemberCategory = 'Pleno' | 'Cadete' | 'Honorario';
 export type MemberStatus = 'Activo' | 'Moroso' | 'Suspendido';
 
 export interface MemberDTO {
-  id: string; // UUID
-  dni: string;
-  name: string;
-  email: string;
-  birthdate: string; // ISO Date String (YYYY-MM-DD)
-  category: MemberCategory;
-  status: MemberStatus;
-  created_at: string; // ISO Date String
+    id: string; // UUID
+    dni: string;
+    name: string;
+    email: string;
+    birthdate: string; // ISO Date String (YYYY-MM-DD)
+    category: MemberCategory;
+    status: MemberStatus;
+    created_at: string; // ISO Date String
 }
 
 export interface CreateMemberRequest {
-  dni: string;
-  name: string;
-  email: string;
-  birthdate: string; // ISO Date String (YYYY-MM-DD)
-  category: MemberCategory;
+    dni: string;
+    name: string;
+    email: string;
+    birthdate: string; // ISO Date String (YYYY-MM-DD)
+    category: MemberCategory;
 }
 
 export interface UpdateMemberRequest {
-  dni?: string;
-  name?: string;
-  email?: string;
-  birthdate?: string; // ISO Date String (YYYY-MM-DD)
-  category?: MemberCategory;
-  status?: MemberStatus;
+    dni?: string;
+    name?: string;
+    email?: string;
+    birthdate?: string; // ISO Date String (YYYY-MM-DD)
+    category?: MemberCategory;
+    status?: MemberStatus;
+}
+
+// ==========================================
+// Payment
+// ==========================================
+export type PaymentStatus = 'Pending' | 'Paid' | 'Overdue' | 'Canceled';
+
+export interface PaymentDTO {
+    id: string;
+    amount: number;
+    month: number;
+    year: number;
+    status: PaymentStatus;
+    due_date: string; // ISO Date String (YYYY-MM-DD)
+    payment_date: string | null; // ISO Date String (YYYY-MM-DD)
+    deleted_at: string | null; // ISO Date String (YYYY-MM-DD)
+    member_id: string;
+}
+
+export interface CreatePaymentRequest {
+    amount: number;
+    month: number;
+    year: number;
+    due_date: string; // ISO Date String (YYYY-MM-DD)
+    member_id: string;
 }
